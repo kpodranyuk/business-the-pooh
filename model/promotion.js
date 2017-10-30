eaf88e6a9f49591a11f3716528716c3eb64cf6ef
Nikita Biryukov <biryukov.nikita96@mail.ru>
2017-10-31 00:12:39 +0300

    Добавлен начальный интерфейс класса скидок

---
+++ model/promotion.js
@@ -0,0 +1,13 @@
+/**
+ * Класс скидки
+ * @param {number} id - идентификатор скидки 
+ */
+function Promotion(id) {
+    this.id = id;
+    var operationsCount = operationsCount;
+    var percent = 0;
+    var operationsToNext = 0;
+
+}
+
+module.exports = Promotion;
