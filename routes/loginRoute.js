01971708f0d0906049339a2c2d596db482c157b7
Alexey Borzikh <boral660@gmail.com>
2017-10-31 21:10:48 +0300

    Заполнен файл с авторизацией в routes

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/userdb.js');
 
 router.post('/', function(req, res) {
 	
-	res.send("Login route");
+	var login= req.body.login;
+	var password = req.body.password
+	var user= db.loginUser(login,password);
+	if(user==NULL){
+		res.json({success: false, message: 'Не удалось авторизоваться. Пользователь не найден.'});
+	}
+	else
+	{
+		res.json({
+                            success: true,					
+							login: user.login,
+							name: user.name,
+							productType: user.productType,
+							productAmount: user.productAmount,
+							honeyAmount: user.honeyAmount,
+							promotion: [{
+									idPromotion: user.promotion.id
+								},{
+									operationsCount: user.promotion.operationsCount
+								},{
+									operationsToNext: user.promotion.operationsToNext
+								},{
+									percent: user.promotion.percent
+							}]
+                        });
+	}
 });
-
 module.exports = router;
