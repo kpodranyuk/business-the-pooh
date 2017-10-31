d0afdde4a00257e86d13359bc75326c02731298b
Nikita Biryukov <biryukov.nikita96@mail.ru>
2017-10-31 19:58:09 +0300

    Добавлен номер порта в конфигурацию и соединение с БД

--- db/userdb.js
+++ db/userdb.js
@@ -3,6 +3,7 @@ var config = require('./config');
 
 var con = mysql.createConnection({
     host: config[host],
+    port: config[port],
     user: config[user],
     password: config[password],
     database: config[database]
