1ce3a843223a875193f49337c7fbeb0f6d51ad01
Alexey Borzikh <boral660@gmail.com>
2017-11-01 19:56:26 +0300

    Переписан конструктор класса операций

--- model/operation.js
+++ model/operation.js
@@ -2,17 +2,22 @@
  * Класс операций
  * @param {number} id - идентификатор операции
  * @param {string} type - тип операции('B','E','G') 
- * 
+ * @param {Date} datatime - дата и время совершения операции
+ * @param {string} productType - тип продукта('F','B','P','H') 
+ * @param {number} productAmount - количество продуктов для обмена
+ * @param {number} honeyPots - количество горшочков с мёдом
+ * @param {number} honeyCount - количество мёда 
+ * @param {number} comission - комиссия
  */
-function Operation(id, type) {
+function Operation(id, type, datatime, productType, productAmount, honeyPots, honeyCount, comission) {
     this.id = id;
     this.type = type;
-    this.datatime = null;
-    this.productType = "";
-    this.productAmount = 0;
-    this.honeyPost = 0;
-    this.honeyCount = 0;
-    this.comission = 0;
+    this.datatime = datatime;
+    this.productType = productType;
+    this.productAmount = productAmount;
+    this.honeyPots = honeyPots;
+    this.honeyCount = honeyCount;
+    this.comission = comission;
 
 }
 
