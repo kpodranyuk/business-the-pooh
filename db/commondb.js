70e9735b622475a0d54a51dc78f507e2655da895
Alexey Borzikh <boral660@gmail.com>
2017-11-01 21:53:57 +0300

    В API написана начальная версия функции получения всех операций

--- db/commondb.js
+++ db/commondb.js
@@ -41,3 +41,5 @@ function getAllHistory(loginUser, callback) {
         });
     });
 }
+
+module.exports.getAllHistory = getAllHistory;
--- routes/commonApiRoute.js
+++ routes/commonApiRoute.js
@@ -1,21 +1,50 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db/commondb.js');
 
 /**
  * Все совершенные операции
  */
-router.post('/operations', function(req, res) {
-	
-	res.send("Operations User or Pooh");
+router.post('/operations', function (req, res) {
+
+	var login = req.body.login;
+	db.getAllHistory(login, function (result) {
+		if (result == null) {
+			res.json({ success: false, message: 'Не удалось получить историю операций данного пользователя' });
+		}
+		else {
+			res.json({
+				success: true
+			});
+
+			for (var i = 0; i < result.length; i++) {
+				var operation = new Operation(
+					result[i].idOperation,
+					result[i].type,
+					result[i].date,
+					result[i].idProductType,
+					result[i].productAmount,
+					result[i].honeyPots,
+					result[i].honeyCount,
+					result[i].comission
+				);
+
+				res.json({
+					user: operation
+				});
+
+			}
+		}
+	});
+
 });
 
 
 /**
  * Вывод меда из системы
  */
-router.post('/get-honey', function(req, res) {
-	
+router.post('/get-honey', function (req, res) {
+
 	res.send("Get honey User or Pooh");
 });
 
