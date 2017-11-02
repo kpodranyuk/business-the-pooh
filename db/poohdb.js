1a1052d3c89b8c4078a8ecafeb52328b32298a64
Alexey Borzikh <boral660@gmail.com>
2017-11-02 19:12:38 +0300

    Написана функция выполняющая запрос всей истории

--- db/poohdb.js
+++ db/poohdb.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var config = require('./config');
+var date = require('../model/operationday');
 
 var con = mysql.createConnection({
     host: config["host"],
@@ -19,10 +20,51 @@ con.connect(function (error) {
 
 /**
  * Получить историю операций за прошлый день
- * @param {string} loginUser - логин пользователя
  * @param {function} функция, отправляющая историю пользователей за прошедший операционный день
  */
-function getHistoryForLastDay(loginUser, callback) {
+function getHistoryForLastDay(callback) {
+    // Начинаем транзакцию 
+    con.beginTransaction(function (err) {
+        if (err) { throw err; }
+
+
+        // Проверить получил ли пух зарплату за текущий оп день
+        var day = new OpeartionDay(new Date());
+        var sql = "SELECT * FROM operation o LEFT OUTER JOIN deal d ON o.idOperation = d.idOperation"
+            + " where d.loginUser=\"superpooh\" AND  o.type= \"E\""
+            + "AND o.date >=" + day.startDay.toLocalestring()
+            + "AND o.date <=" + day.endDay.toLocalestring();
+        // Если получил
+        con.query(sql, function (error, result, fields) {
+            if (error) {
+                callback(null);
+                return con.rollback(function () { console.error(error.message); });
+            } else {
+                if (result != null) {
+                    // Отправить пустой список
+                    callback(null);
+                }
+                else {
+                    var sql = "SELECT o.date, d.loginUser, o.comission"
+                        + "FROM operation o LEFT OUTER JOIN deal d"
+                        + " ON o.idOperation = d.idOperation where o.type=\"B\"";
+                        
+                    // Получить из бд все операции с типом покупка
+                    con.query(sql, function (error, result, fields) {
+                        if (error) {
+                            callback(null);
+                            return con.rollback(function () { console.error(error.message); });
+                        }
+                        callback(result);
+                        con.commit(function (error) {
+                            if (error) return con.rollback(function () { console.error(error.message); });
+                        });
+
+                    });
+                }
+            }
+        });
+    });
 }
 
 module.exports.getHistoryForLastDay = getHistoryForLastDay;
