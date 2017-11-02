2cbe7946c1f891488a83e872f287fbdfb916200c
Alexey Borzikh <boral660@gmail.com>
2017-11-02 21:09:16 +0300

    Написана первая версия функции получающей историю

--- routes/poohApiRoute.js
+++ routes/poohApiRoute.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/poohdb.js');
 
 
 /**
  * Операции пользователей за прошлый операционный день
  */
-router.post('/last-op-day', function(req, res) {
-	
-	res.send("Pooh look operations the last day");
+router.post('/last-op-day', function (req, res) {
+	db.getHistoryForLastDay(function (result) {
+		if (result == null) {
+			res.json({ success: true });
+		}
+		else {
+			res.json({
+				success: true,
+				result: result
+			});
+		}
+	});
 });
 
 
-/**
- * Операции пользователей за прошлый операционный день
- */
-router.post('/get-commission', function(req, res) {
-	
-	res.send("Pooh get commission from users");
-});
+	/**
+	 * Операции пользователей за прошлый операционный день
+	 */
+	router.post('/get-commission', function (req, res) {
+
+		res.send("Pooh get commission from users");
+	});
 
 
-module.exports = router;
+	module.exports = router;
