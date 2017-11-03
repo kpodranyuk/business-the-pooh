46531cb8735f55a97d22a8a52f0c30544382ecbf
Alexey Borzikh <boral660@gmail.com>
2017-11-03 20:58:41 +0300

    Добавлен текст запроса информации о пользователе после вывода

--- routes/commonApiRoute.js
+++ routes/commonApiRoute.js
@@ -59,34 +59,35 @@ router.post('/operations', function (req, res) {
  * Вывод меда из системы
  */
 router.post('/get-honey', function (req, res) {
-	
-		// res.send("Get honey User or Pooh");
-		// Получаем логин пользователя
-		var login = req.body.login;
-		// Получаем желаемое для снятия количество меда
-		var honey = req.body.honey;
-		// Говорим БД обновить данные пользователя
-		db.withdrawUserHoney(login, honey, function (result) {
-			if (result == null) {
-				res.json({ success: false, message: 'Не удалось снять мед у данного пользователя' });
-			}
-			else if(result.affectedRows == 0){
-				//console.log(result);
-				res.json({
-					success: false,
-					message: 'Запись пользователя не была найдена'
-				});
-			}
-			else{
-				// console.log(result);
-				res.json({
-					success: true,
-					message: 'Мед был выведен из системы'
-				});
-			}
-		});
+
+	// res.send("Get honey User or Pooh");
+	// Получаем логин пользователя
+	var login = req.body.login;
+	// Получаем желаемое для снятия количество меда
+	var honey = req.body.honey;
+	// Говорим БД обновить данные пользователя
+	db.withdrawUserHoney(login, honey, function (result) {
+		if (result == null) {
+			res.json({ success: false, message: 'Не удалось снять мед у данного пользователя' });
+		}
+		else if (result.affectedRows == 0) {
+			//console.log(result);
+			res.json({
+				success: false,
+				message: 'Запись пользователя не была найдена'
+			});
+		}
+		else {
+			// console.log(result);
+			res.json({
+				success: true,
+				message: 'Мед был выведен из системы'
+			});
+		}
 	});
-	
+});
+
+
 
 
 /**
@@ -94,9 +95,53 @@ router.post('/get-honey', function (req, res) {
  */
 router.post('/get-honey-info', function (req, res) {
 
-	res.send("Get honey User or Pooh info");
+	// res.send("Get honey User or Pooh info");	
+	// Получаем логин пользователя
+	var login = req.body.login;
+	// Получаем у бд баланс пользователя
+	db.getUserBalance(login, function (result) {
+		if (result == null) {
+			res.json({ success: false, message: 'Не удалось получить баланс данного пользователя' });
+		}
+		else {
+			var honeyCount = result.honeyCount;	// Количество меда у пользователя
+			var comissioned = 0;	// Мед "под комиссией" - его нельзя снять
+			var canGet = 0;			// Мед, который можно вывести
+			var alreadyGot = 0;		// Мед, который уже был выведен за сегодня
+			db.getTodaysOperations(login, function (result) {
+				if (result == null) {
+					res.json({ success: false, message: 'Не удалось получить историю операций за текущий день у данного пользователя' });
+				}
+				else {
+					// Для каждой операции..					
+					for (var i = 0; i < result.length; i++) {
+						// Определить "комиссионный мед", который трогать нельзя
+						comissioned += result[i].comission;
+						// Определить количество уже выведенного сегодня меда
+						if (result[i].type == 'G')
+							alreadyGot += result[i].honeyCount;
+					}
+					// Определяем свободное количество меда
+					canGet = honeyCount - comissioned;
+					if (canGet > 5.0) {
+						canGet = 5.0;
+					}
+					// Определить допустимое количество меда для вывода - в сумме за день не более 5л
+					if (alreadyGot == 5.0) {
+						canGet = 0;
+					}
+					else if (alreadyGot + canGet >= 5.0) {
+						canGet = 5.0 - alreadyGot;
+					}
+					res.json({
+						success: true,
+						honeyToGet: canGet
+					});
+				}
+			});
+		}
+	});
 });
-
 
 
 module.exports = router;
