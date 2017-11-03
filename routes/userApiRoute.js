2862de652424f85b0589e628a4676a093e88d9e5
Alexey Borzikh <boral660@gmail.com>
2017-11-03 21:24:42 +0300

    Добавлена функция осуществляющая ввод своего товара в систему

--- routes/userApiRoute.js
+++ routes/userApiRoute.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db/userdb.js');
 
 /**
  * Покупка меда(подтверждение)
  */
-router.post('/buy-honey', function(req, res) {
-	
+router.post('/buy-honey', function (req, res) {
+
 	res.send("User buy honey(confirm)");
 });
 
@@ -14,26 +14,62 @@ router.post('/buy-honey', function(req, res) {
 /**
  * Покупка меда(информация при покупке: скидка, комиссия и тд)
  */
-router.post('/buy-honey-info', function(req, res) {
-	
+router.post('/buy-honey-info', function (req, res) {
+
 	res.send("User buy honey(info)");
 });
+
 
 
 /**
  * Ввод своего товара в систему
  */
-router.post('/entry-product', function(req, res) {
-	
-	res.send("User entry product");
+router.post('/entry-product', function (req, res) {
+
+	// res.send("User entry product");
+	// Получаем логин пользователя
+	var login = req.body.login;
+	// Получаем желаемое для ввода количество товара
+	var product = req.body.product;
+	// Говорим БД обновить данные пользователя
+	db.enterUserProduct(login, product, function (result) {
+		if (result == null) {
+			res.json({ success: false, message: 'Не удалось ввести товар для данного пользователя' });
+		}
+		else if (result.affectedRows == 0) {
+			//console.log(result);
+			res.json({
+				success: false,
+				message: 'Запись пользователя не была найдена'
+			});
+		}
+		else {
+			db.getUserBalance(login, function (result) {
+				if (result == null) {
+					res.json({ success: false, message: 'Не удалось получить баланс пользователя' });
+				}
+				else {
+					// Выводим баланс пользователя
+					res.json({
+						success: true,
+						productAmount: result.productAmount,
+						honeyAmount: result.honeyAmount,
+						idProductType: result.idProductType
+					});
+				}
+
+			});
+		}
+	});
 });
+
 
 
 /**
  * Ввод своего товара в систему(информация)
  */
-router.post('/entry-product-info', function(req, res) {
-	
+router.post('/entry-product-info', function (req, res) {
+
 	res.send("User entry product(info)");
 });
 
@@ -41,8 +77,8 @@ router.post('/entry-product-info', function(req, res) {
 /**
  * Деактивация аккаунта
  */
-router.post('/deactivate-account', function(req, res) {
-	
+router.post('/deactivate-account', function (req, res) {
+
 	res.send("User deactivate account");
 });
 
@@ -50,8 +86,8 @@ router.post('/deactivate-account', function(req, res) {
 /**
  * Обновление пароля
  */
-router.post('/password-update', function(req, res) {
-	
+router.post('/password-update', function (req, res) {
+
 	res.send("User password update");
 });
 
