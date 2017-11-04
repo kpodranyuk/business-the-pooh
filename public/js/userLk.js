a94c390ce7a18410ca3004d7673611a9990196c1
kpodranyuk <katie.katie2011@yandex.ru>
2017-11-04 13:21:37 +0300

    Добавлены повороты изображения пчел на вкладках ввода товара и вывода меда

--- public/js/userLk.js
+++ public/js/userLk.js
@@ -1,5 +1,43 @@
+// Переключение на скрытую вкладку с настройками аккаунта
 var acc_bttn = document.querySelector("#cont");
-console.log(acc_bttn)
+console.log(acc_bttn);
 acc_bttn.onclick=function(event){
     $('#pills a[href="#trueAcc"]').tab('show')
 }
+
+// Изображение пчелы при выводе меда
+var bee_out = document.querySelector("#outbee");
+console.log(bee_out);
+
+// Изображение пчелы при вводе товара
+var bee_in = document.querySelector("#inbee");
+console.log(bee_in);
+
+// Анимация пчелы при вводе товара
+var inAnime = anime({
+    targets: bee_in,
+    rotate: '1turn',
+    autoplay: false,
+    direction: 'reverse',
+    complete: function(anim) {
+        // СОБЫТИЕ ЗАВЕРШЕНИЯ АНИМАЦИИ ПРИ ВВОДЕ ТОВАРА
+      }
+});
+
+// Анимация пчелы при выводе меда
+var outAnime = anime({
+    targets: bee_out,
+    rotate: '1turn',
+    autoplay: false,
+    complete: function(anim) {
+        // СОБЫТИЕ ЗАВЕРШЕНИЯ АНИМАЦИИ ПРИ ВЫВОДЕ МЕДА
+      }
+});
+
+// Кнопка вывода меда
+var out_bttn = document.querySelector("#honeyOut");
+out_bttn.onclick=outAnime.restart;
+
+// Кнопка ввода товара
+var in_bttn = document.querySelector("#honeyIn");
+in_bttn.onclick=inAnime.restart;
