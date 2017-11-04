c47e309f86422cd8ae428601e8fd074fc14f0140
kpodranyuk <katie.katie2011@yandex.ru>
2017-11-04 15:12:58 +0300

    Добавлена проверка количества выводимого меда в личном кабинете Винни Пуха.

--- public/js/poohLk.js
+++ public/js/poohLk.js
@@ -7,9 +7,18 @@ var outAnime = anime({
     targets: beeOut,
     rotate: '1turn',
     autoplay: false,
-    complete: function(anim) {
+    duration: 800,
+    begin: function(anim) {
         // СОБЫТИЕ ЗАВЕРШЕНИЯ АНИМАЦИИ ПРИ ВЫВОДЕ МЕДА
         console.log("Нажата кнопка вывода меда");
+        // Проверить введенное количество меда
+        var honeyCount = document.querySelector("#honeyInput");
+        console.log(honeyCount);
+        var honeyCountHelp = document.querySelector("#honeyInputHelp");
+        console.log(honeyCountHelp);
+        var wasPapaProud = false;
+        wasPapaProud = isCorrectHoneyAmount(honeyCount.value, honeyCountHelp);
+        makePapaProud(honeyCount.parentNode, wasPapaProud);
       }
 });
 
@@ -27,4 +36,42 @@ comissionButton.onclick = function(event){
 var logOutBttn = document.querySelector("#logOut");
 logOutBttn.onclick = function(event){
     console.log("Нажата кнопка выхода из аккаунта");
+}
+
+function makePapaProud(parentForm, isProud){
+    if(isProud){
+        parentForm.classList.remove("has-error");
+        parentForm.classList.add("has-success");
+        return true;
+    }
+    else{
+        parentForm.classList.remove("has-success");
+        parentForm.classList.add("has-error");
+        return false;
+    }
+}
+
+function isCorrectHoneyAmount(honeyAmount, errorPlace){
+    console.log("I'm in isCorrectHoneyAmount");
+    var reg = new RegExp(`^[0-5]([.,][0-9]{1,3})?$`, '');
+    if (honeyAmount==null){
+        console.log("honeyAmount is null");
+        errorPlace.innerHTML = "Введите количество меда, пустое поле";
+        return false;
+    } 
+    if(reg.test(honeyAmount)){
+        if (parseFloat(honeyAmount)<0.005 || parseFloat(honeyAmount)>5.0){
+            console.log("wrong amount of honeyAmount");
+            errorPlace.innerHTML = "Количество меда не должно быть меньше 0.005 и больше 5.0";
+            return false;
+        }
+        console.log("true, correct honeyAmount");
+        errorPlace.innerHTML = "Корректное количество меда";
+        return true;
+    }
+    else {
+        console.log("wrong, incorrect honeyAmount");
+        errorPlace.innerHTML = "Некорректное количество меда.<br>Количество меда должно быть положительным числом меньше 5";
+        return false;
+    }    
 }
