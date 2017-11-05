ae7d461ba8140064cabfbb8c3c5ea5e1f9aba398
kpodranyuk <katie.katie2011@yandex.ru>
2017-11-05 18:36:35 +0300

    Добавлена обработка запроса регистрации пользователя.

--- public/js/indexApi.js
+++ public/js/indexApi.js
@@ -1,3 +1,57 @@
 export function sayHiGuest() {
-    alert("Hello, Guest!");
+    //alert("Hello, Guest!");
 };
+
+/**
+ * Отправить на сервер запрос о регистрации пользователя
+ */
+export function sendRegist(){
+    // Формируем запрос
+    var req = $.ajax({
+        method: "POST",
+        url: '/register',
+        header: {
+            "Content-Type": 'application/json',
+        },        
+        dataType: 'json',
+        data: {
+            login : $("#regInputLogin").val(),
+            password : $("#regInputPassRepeat").val(),
+            name : $("#regInputName").val(),
+            productType : convertUserTypeToString()
+        },   
+        success: function(response){
+            if(response.success == true){
+                console.log("ПОЛЬЗОВАТЕЛЬ ЗАРЕГИСТРИРОВАН!!!");
+                // Запоминаем пользователя в браузере
+                localStorage.setItem('currentUser', JSON.stringify(response.user));
+                // Переходим на страницу личного кабинета
+                window.location = "userLk.html"
+            }
+            else{
+                console.log(response.message);
+                // TODO сделать вывод сообщения в поле под логином
+            }
+        },
+        error: function(response){
+            console.log("ОШИБКА ПРИ РЕГИСТРАЦИИ");
+            console.log(response);
+        }, 
+        complete: function(){
+
+        }        
+    });
+}
+
+/**
+ * Конвертировать выбранный пользователем тип для хранения в БД
+ */
+function convertUserTypeToString(){
+    var val = $("#userType").val();
+    if (val == "rabbit")
+        return "F";
+    if (val == "owl")
+        return "P";
+    if (val == "pig")
+        return "B";
+}
--- public/js/userLkApi.js
+++ public/js/userLkApi.js
@@ -1,3 +1,13 @@
 export function sayHiUser() {
     alert("Hello, user!");
 };
+
+// По загрузке документа заполняем элементы, отображающие информацию о пользователе
+$(document).ready(function(){
+       // TODO установить информацию о пользователе
+       // TODO сделать функцию для работы с localStorage
+       var a = [];
+       // Проверяем работу localStorage
+       a = JSON.parse(localStorage.getItem('currentUser'));
+       console.log(a.name);
+});
