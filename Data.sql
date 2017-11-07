2e26c2c0fa2c0de2925bb9b6a8874c5511441759
Alexey Borzikh <boral660@gmail.com>
2017-11-07 16:27:48 +0300

    Измененны тестовые данные

--- Data.sql
+++ Data.sql
@@ -1,6 +1,6 @@
 # Пользователи
 INSERT INTO User(login, password, name, isAdmin, productAmount, honeyAmount, idProductType, idPromotion) 
-VALUES('super_owl44', 'yasovaatinet', 'Екатерина', false, 0, 0, (select idProductType from ProductType where type='P'), 1);
+VALUES('super_owl44', 'yasovaatinet', 'Екатерина', false, 15, 5, (select idProductType from ProductType where type='P'), 1);
 INSERT INTO User(login, password, name, isAdmin, productAmount, honeyAmount, idProductType, idPromotion) 
 VALUES('P9ltA40k', 'pyatak2828', 'Дмитрий', false, 0, 0, (select idProductType from ProductType where type='B'), 1);
 INSERT INTO User(login, password, name, isAdmin, productAmount, honeyAmount, idProductType, idPromotion) 
@@ -14,35 +14,35 @@ VALUES('rraabbiitt', 'aaaaa1234', 'Евстрат', false, 0, 0, (select idProdu
 # Krolic										
 #1
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('E', '2017-10-31 08:20:43', 100, 0, 0, 0, (select idProductType from ProductType where type='F'));
+VALUES('E', '2017-11-05 08:20:43', 45, 0, 0, 0, (select idProductType from ProductType where type='F'));
 #2
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('B', '2017-10-31 15:30:43', 90, 9, 2.25, 0.3375, (select idProductType from ProductType where type='F'));
+VALUES('B', '2017-11-05 15:30:43', 40, 4, 1, 0.15, (select idProductType from ProductType where type='F'));
 #3
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('G', '2017-10-31 22:55:43', 0, 0,  0.5, 0, (select idProductType from ProductType where type='H'));
+VALUES('G', '2017-11-05 22:55:43', 0, 0,  0.5, 0, (select idProductType from ProductType where type='H'));
 
 # Petya	
 #4
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('E', '2017-11-1 03:22:43', 10, 0, 0, 0, (select idProductType from ProductType where type='B'));
+VALUES('E', '2017-11-6 03:22:43', 10, 0, 0, 0, (select idProductType from ProductType where type='B'));
 #5
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('B', '2017-11-1 15:35:43', 10, 1, 0.25, 0.0375, (select idProductType from ProductType where type='B'));
+VALUES('B', '2017-11-6 15:35:43', 10, 1, 0.25, 0.0375, (select idProductType from ProductType where type='B'));
 #6
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('G', '2017-11-2 16:30:43', 0, 0,  0.2125, 0, (select idProductType from ProductType where type='H'));
+VALUES('G', '2017-11-7 13:30:43', 0, 0,  0.2125, 0, (select idProductType from ProductType where type='H'));
 
 # Sova
 #7
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('E', '2017-10-31 22:25:00', 50, 0, 0, 0, (select idProductType from ProductType where type='B'));
+VALUES('E', '2017-10-5 22:25:00', 50, 0, 0, 0, (select idProductType from ProductType where type='B'));
 #8
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('B', '2017-11-1 6:30:43', 15, 3, 0.75, 0.1125, (select idProductType from ProductType where type='B'));
+VALUES('B', '2017-11-7 6:30:43', 15, 3, 0.75, 0.1125, (select idProductType from ProductType where type='B'));
 #9
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('G', '2017-11-1 6:31:35', 0, 0,  0.63, 0, (select idProductType from ProductType where type='H'));
+VALUES('G', '2017-11-7 6:31:35', 0, 0,  0.63, 0, (select idProductType from ProductType where type='H'));
 
 # КРОЛИКИ
 INSERT INTO Deal(loginUser, idOperation) VALUES('rraabbiitt', 1);
@@ -61,4 +61,4 @@ INSERT INTO Deal(loginUser, idOperation) VALUES('super_owl44', 9);
 
 # Винни
 INSERT INTO Operation(type, date, productAmount, honeyPots, honeyCount, comission, idProductType) 
-VALUES('E', '2017-11-1 22:1:43', 0, 0,  0.4480, 0, (select idProductType from ProductType where type='H'));
+VALUES('E', '2017-11-1 22:1:43', 0, 0,  0.15, 0, (select idProductType from ProductType where type='H'));
