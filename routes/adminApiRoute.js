6dc61b320b4cb44a37c6f2673673139c900da104
Nikita Biryukov <biryukov.nikita96@mail.ru>
2017-12-12 00:40:55 +0300

    Добавлены заглушки всех API вызовов для администратора.

--- routes/adminApiRoute.js
+++ routes/adminApiRoute.js
@@ -2,6 +2,68 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+/**
+ * Получение информации о текущем курсе товаров
+ */
+router.get('/exchange-rate-info', function (req, res) {
+
+});
+
+
+/**
+ * Отредактировать товар
+ */
+router.get('/edit-product', function (req, res) {
+
+});
+
+
+/**
+ * Получить информацию о всех типах пользователей
+ */
+router.get('/user-type-info', function (req, res) {
+
+});
+
+
+/**
+ * Отредактировать определенный тип пользователя
+ */
+router.get('/user-type-edit', function (req, res) {
+
+});
+
+
+/**
+ * Удалить определенный тип пользователя
+ */
+router.get('/user-type-delete', function (req, res) {
+    
+});
+
+
+/**
+ * Добавить определенный тип пользователя
+ */
+router.get('/user-type-add', function (req, res) {
+    
+});
+
+
+/**
+ * Редактирование кол-ва выпускаемого пчелами меда в сутки
+ */
+router.get('/edit-pots-count', function (req, res) {
+    
+});
+
+
+/**
+ * Редактирование системы поощерения для новых пользователей
+ */
+router.get('/edit-promotion', function (req, res) {
+    
+});
 
 
 module.exports = router;
