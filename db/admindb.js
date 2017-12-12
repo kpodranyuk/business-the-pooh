35c205c6b0aa16a3638eb8b35749dcc82e1de2d9
Alexey Borzikh <boral660@gmail.com>
2017-12-12 12:35:25 +0300

    Написан заголовок функции выполняющей запрос на редактирование товара

--- db/admindb.js
+++ db/admindb.js
@@ -5,7 +5,20 @@ var con = require("./connection");
  * Получение информации о текущем курсе товаров
  * @param {function} функция, отправляющая информацию о курсе
  */
-function getExchangeRateInfo( callback) {
+function getExchangeRateInfo(callback) {
 
 }
+
+/**
+ * Отредактировать товар
+ * @param {string} productName - название товара
+ * @param {string} newProductName - новое название товара
+ * @param {int} newExchangeRate - новый курс обмена
+ * @param {function} функция, отправляющая новую информацию о товаре
+ */
+function editProduct(productName, newProductName, newExchangeRate, callback) {
+
+}
+
+module.exports.editProduct = editProduct;
 module.exports.getExchangeRateInfo = getExchangeRateInfo;
