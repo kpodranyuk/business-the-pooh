9a89a045fe14ed34f8a3dae77a2c13b5a6bf2a8f
Alexey Borzikh <boral660@gmail.com>
2017-12-12 12:21:08 +0300

    Написан заголовок функции для получения информации о курсе из базы данных

--- db/admindb.js
+++ db/admindb.js
@@ -1,2 +1,11 @@
 var mysql = require('mysql');
 var con = require("./connection");
+
+/**
+ * Получение информации о текущем курсе товаров
+ * @param {function} функция, отправляющая информацию о курсе
+ */
+function getExchangeRateInfo( callback) {
+
+}
+module.exports.getExchangeRateInfo = getExchangeRateInfo;
