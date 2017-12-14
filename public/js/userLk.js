f73dd74fcd94c5bd3b3246abab0d0ecc1d27dfe8
Nikita Biryukov <biryukov.nikita96@mail.ru>
2017-12-14 20:26:08 +0300

    исправлены теперь все недочеты при деактивации аккаунта.

--- public/js/userLk.js
+++ public/js/userLk.js
@@ -481,9 +481,13 @@ forgetMeBttn.onclick = function(event){
 		if(pswdInput.value == userApi.curUser.password){
             console.log("Деактивация");
             userApi.deactivateAccount(function (success) {
-                socket.emit('leave', { username: userApi.curUser.login });
-                localStorage.clear();
-                window.location = "/";
+                if (success) {
+                    socket.emit('leave', { username: userApi.curUser.login });
+                    localStorage.clear();
+                    window.location = "/";
+                } else {
+                    alert("Не удалось деактивировать аккаунт!");
+                }
             });
 		}
 		else{
