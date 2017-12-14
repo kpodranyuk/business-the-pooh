f715cec05ab28b785324c846d18f2fd1676da5ae
Nikita Biryukov <biryukov.nikita96@mail.ru>
2017-12-14 17:15:36 +0300

    Исправлен вход в ЛК у администратора. Исправлена подгрузка типов пользователей при регистрации.

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -8,18 +8,25 @@ router.post('/', function (req, res) {
 	var login = req.body.login;
 	var password = req.body.password;
 	db.loginUser(login, password, function (success) {
-		if (success == null) {
+		if (success == null && !(login == "admin" && password == "adminadmin")) {
 			res.json({ success: false, message: 'Не удалось авторизоваться. Пользователь не найден.' });
 		} else if (success) {
 			res.json({ success: false, message: 'Данный пользователь деактивирован.' });
 		} else {
-			// Получить все данные о пользователе и отправить их на клиент
-			dbc.getUser(login, function (user) {
+			if (login == "admin" && password == "adminadmin") {
 				res.json({
 					success: true,
-					user: user
+					user: "admin"
 				});
-			});
+			} else {
+				// Получить все данные о пользователе и отправить их на клиент
+				dbc.getUser(login, function (user) {
+					res.json({
+						success: true,
+						user: user
+					});
+				});
+			}
 		}
 	});
 });
