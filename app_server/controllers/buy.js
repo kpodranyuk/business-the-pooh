a13ccb26c77ed175a6870fe48c7ce8f6bb722ffa
Nikita Biryukov <biryukov.nikita96@mail.ru>
2018-05-12 14:57:26 +0300

    Добавлена апишка для покупки.

---
+++ app_server/controllers/buy.js
@@ -0,0 +1,62 @@
+var db = require('../models/model.js');
+
+/**
+ * Функция отправки данных на клиент
+ * @param {*} res объект ответа на клиент
+ * @param {JSON} content js объект который идет на клиент
+ */
+var sendJSONresponse = function (res, content) {
+    res.json(content);
+};
+
+module.exports.buy = function (req, res) {
+
+    var login = req.body.login;
+    var countEntryProduct = req.body.countEntryProduct;
+    var entryProduct = req.body.entryProduct;
+    var countOutputProduct = req.body.countOutputProduct;
+    var outputProduct = req.body.outputProduct;
+
+    db.Purse.findAll({
+        where: { loginUser: login },
+        attributes: ['amount', 'itemType']
+    }).then(purse => {
+        // рассчитываем новый кошелек
+        for (var i = 0; i < purse.length; i++) {
+            if (outputProduct == purse[i].itemType) {
+                purse[i].amount += countOutputProduct;
+            } else if (entryProduct == purse[i].itemType) {
+                purse[i].amount -= countEntryProduct;
+            }
+        }
+        return purse;
+    }).then(purse => {
+        // обновим кошелек пользователя
+        for (var i = 0; i < purse.length; i++) {
+            db.Purse.update(
+                { amount: purse[i].amount },
+                { where: { [db.op.and]: [{ loginUser: login }, { itemType: purse[i].itemType }] } }
+            ).then(() => { });
+        }
+
+        // Добавим новую операцию
+        var typeOperation;
+        if (outputProduct == 'honey')
+            typeOperation = 'exchangeForestProductForHoney';
+        else if (outputProduct != 'honey' && outputProduct != 'ruble')
+            typeOperation = 'exchangeForForestProduct';
+        db.Operation.create({
+            typeOperation: typeOperation,
+            date: new Date(),
+            countEntryProduct: countEntryProduct,
+            entryProduct: entryProduct,
+            countOutputProduct: countOutputProduct,
+            outputProduct: outputProduct
+        }).then(purse => {
+            sendJSONresponse(res, {
+                ok: true,
+                purse: purse
+            });
+        });
+    });
+};
--- app_server/routes/router.js
+++ app_server/routes/router.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var loginAndRegister = require('../controllers/loginAndRegister.js');
+var buy = require('../controllers/buy.js')
 var userHistory = require('../controllers/userHistory.js');
 
 router.post('/login', loginAndRegister.login);
 router.post('/register', loginAndRegister.register);
+router.post('/buy', buy.buy);
 router.post('/history', userHistory.history);
 
 
